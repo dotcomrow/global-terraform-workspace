b888a6fe30c3a0a400212884014e0eba161a4e5d
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-10-01 17:28:17 -0400

    cleanup
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,10 +10,6 @@ variable credentials_file {
     default = "google.key"
 }
 
-variable dataset_name {
-    default = "<DATASET_NAME>"
-}
-
 variable gcp_org_id {
     default = "<GCP_ORGANIZATION_ID>" 
 }
