3d0e616fe118a8799be4c0b5e3371fb2c1dbf92c
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-10-01 19:40:09 -0400

    ?
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/service_accounts.tf
+++ terraform/service_accounts.tf
@@ -10,7 +10,7 @@ module "gcloud" {
   platform = "linux"
 #   additional_components = ["kubectl", "beta"]
 
-  create_cmd_entrypoint  = "gcloud"
+#   create_cmd_entrypoint  = "gcloud"
   create_cmd_body        = "services enable iam.googleapis.com serviceusage.googleapis.com cloudresourcemanager.googleapis.com --project suncoast-systems-products"
 #   destroy_cmd_entrypoint = "gcloud"
 #   destroy_cmd_body       = "version"
