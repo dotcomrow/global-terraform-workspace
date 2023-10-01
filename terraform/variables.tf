3d1030409270dd04ea01cf695acc81191f909e03
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-10-01 15:11:13 -0400

    Initial commit

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "<YOUR-PROJECT-ID>"
+}
+
+variable "region" {
+    default = "<REGION>"
+}
+
+variable credentials_file {
+    default = "google.key"
+}
+
+variable dataset_name {
+    default = "<DATASET_NAME>"
+}
