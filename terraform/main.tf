37ca063cc66301265d1a9988a87bc02094ce87bd
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-10-01 19:18:43 -0400

    adding gcloud
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,8 @@ provider "google" {
   region  = var.region
   credentials = file(var.credentials_file) 
 }
+
+module "gcloud" {
+  source  = "terraform-google-modules/gcloud/google"
+  version = "3.2.1"
+}
