0f0e57fa823d70e800ccea579400c0fc6d8bb709
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-10-01 15:58:03 -0400

    ?
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/projects.tf
+++ terraform/projects.tf
@@ -1,5 +1,5 @@
 resource "google_project" "my_project_12341234" {
   name       = "My Project"
-  project_id = "123134123412341234"
+  project_id = "jgfdgfd-7856786"
   org_id     = "${var.gcp_org_id}"
 }
