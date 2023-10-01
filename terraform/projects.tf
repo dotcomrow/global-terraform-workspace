dd3fe8b6ebf463e4015650be0d1755de06741636
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-10-01 17:23:48 -0400

    products project
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/projects.tf
+++ terraform/projects.tf
@@ -1,5 +1,5 @@
-resource "google_project" "my_project_12341234" {
-  name       = "My Project"
-  project_id = "jgfdgfd-7856786"
+resource "google_project" "products" {
+  name       = "products"
+  project_id = "suncoast.systems-products"
   org_id     = "${var.gcp_org_id}"
 }
