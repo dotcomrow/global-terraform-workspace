938ccc6fc6910f5cb8534184ac2919d6d9ddc59f
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-10-01 15:54:41 -0400

    ?
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "<YOUR-PROJECT-ID>"
+}
+
+variable "region" {
+    default = "<REGION>"
+}
+
+variable credentials_file {
+    default = "google.key"
+}
+
+variable dataset_name {
+    default = "<DATASET_NAME>"
+}
+
+variable gcp_org_id {
+    default = "<GCP_ORGANIZATION_ID>" 
+}
