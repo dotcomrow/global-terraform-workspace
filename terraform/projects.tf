f38e573aae903e021bf8cc5fb34812fe5f77b032
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-10-02 12:18:55 -0400

    adding project resources
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/projects.tf
+++ terraform/projects.tf
@@ -3,3 +3,19 @@ resource "google_project" "products" {
   project_id = "suncoast-systems-products"
   org_id     = "${var.gcp_org_id}"
 }
+
+resource "google_project_service" "products" {
+  project = google_project.products.project_id
+  service = "iam.googleapis.com"
+}
+
+resource "google_project_service" "products2" {
+  project = google_project.products.project_id
+  service = "cloudresourcemanager.googleapis.com"
+}
+
+resource "google_project_service" "products3" {
+  project = google_project.products.project_id
+  service = "cloudbilling.googleapis.com"
+}
+
