d3fc8263e1c4f87aefd2396adc0908cc9da0d2ff
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-10-02 12:25:31 -0400

    name fix
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/projects.tf
+++ terraform/projects.tf
@@ -1,6 +1,6 @@
 resource "google_project" "products" {
   name       = "products"
-  project_id = "suncoast-systems-products-domain"
+  project_id = "suncoast-systems-products-dom"
   org_id     = "${var.gcp_org_id}"
 }
 
