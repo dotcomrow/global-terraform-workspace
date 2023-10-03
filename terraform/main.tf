33a6c4b8a99da44386914ffbfc9d9f4ee1b669bc
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-10-03 17:55:46 -0400

    adding host key
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,12 @@ provider "google" {
   credentials = file(var.credentials_file) 
 }
 
+resource "null_resource" "add_host_key" {
+  provisioner "local-exec" {
+    command = "ssh-keyscan github.com >> ~/.ssh/known_hosts"
+  }
+}
+
 resource "null_resource" "git_clone_products" {
   provisioner "local-exec" {
     command = "git clone git@github.com:dotcomrow/products-terraform-workspace.git"
