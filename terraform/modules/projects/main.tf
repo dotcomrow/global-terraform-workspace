5e270b309631f9a26ba813cf0f88472a6cfdaecc
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-10-03 20:35:52 -0400

    ?
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/modules/projects/main.tf
+++ terraform/modules/projects/main.tf
@@ -2,12 +2,12 @@ resource "google_project" "project" {
   name       = "${var.project_name}"
   project_id = "${var.project_name}"
   org_id     = "${var.gcp_org_id}"
+  disable_dependent_services = true
 }
 
 resource "google_project_service" "project_service" {
   count = length(var.apis)
 
-  disable_dependent_services = true
   project = google_project.project.project_id
   service = var.apis[count.index]
 }
