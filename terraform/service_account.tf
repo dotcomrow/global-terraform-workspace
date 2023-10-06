5a89ace4f74967d1a8582c90eb19b64d42ed091f
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-10-05 22:13:22 -0400

    adding global service account
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

---
+++ terraform/service_account.tf
@@ -0,0 +1,39 @@
+resource "google_service_account" "service_account" {
+  account_id   = "${var.project_name}-cicd"
+  project      = "${var.project_name}"
+  display_name = "${var.project_name} GitHub Actions Service Account"
+}
+
+resource "google_project_iam_binding" "service_account_iam" {
+  project = "${var.project_name}"
+  role    = "roles/editor"
+  members = [
+    "serviceAccount:${google_service_account.service_account.email}"
+  ]
+}
+
+resource "google_service_account_key" "key" {
+  service_account_id = google_service_account.service_account.name
+  
+  keepers = {
+    rotation_time = time_rotating.mykey_rotation.rotation_rfc3339
+  }
+}
+
+resource "google_secret_manager_secret" "service_account" {
+  secret_id = google_service_account.service_account.account_id
+
+  replication {
+    user_managed {
+      replicas {
+        location = var.region
+      }
+    }
+  }
+  project = var.project_name
+}
+
+resource "google_secret_manager_secret_version" "service_account_private_key" {
+  secret = google_secret_manager_secret.service_account.id
+  secret_data = base64decode(google_service_account_key.key.private_key)
+}
