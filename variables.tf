962704662336b707d1a3030cf8e40e6836a00ffd
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-10-07 10:11:24 -0400

    test
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- variables.tf
+++ variables.tf
@@ -2,22 +2,26 @@ variable "common_project_name" {
   nullable = false
   description = "The name of project to store global bigquery service account"
   type = string
+  default = ""
 }
 
 variable "region" {
   description = "The region to deploy resources"
   type        = string
   nullable = false
+  default = ""
 }
 
 variable "gcp_org_id" {
   description = "The GCP organization id"
   type        = string
   nullable = false
+  default = ""
 }
 
 variable "billing_account" {
   description = "The billing account id"
   type        = string
   nullable = false
+  default = ""
 }
