e80a6c2373c978692ce09558ce06ace30e64dfb4
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-10-07 10:17:32 -0400

    test
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -2,6 +2,26 @@ provider "google" {
   region  = "${var.region}"
 }
 
+variable "common_project_name" {
+  description = "The name of project to store global bigquery service account"
+  type = string
+}
+
+variable "region" {
+  description = "The region to deploy resources"
+  type        = string
+}
+
+variable "gcp_org_id" {
+  description = "The GCP organization id"
+  type        = string
+}
+
+variable "billing_account" {
+  description = "The billing account id"
+  type        = string
+}
+
 module "orders" {
   source  = "app.terraform.io/dotcomrow/orders/google"
   version = "> 1.0.0"
