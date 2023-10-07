e59aabd62e8adf488d5a7d955fe7b4d62990b89c
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-10-07 10:22:18 -0400

    update names
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   cloud {
-    organization = "<ORGANIZATION_ID>"
+    organization = "dotcomrow"
 
     workspaces {
-      name = "<WORKSPACE_NAME>"
+      name = "global-terraform-workspace"
     }
   }
 }
