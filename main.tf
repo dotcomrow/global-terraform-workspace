b11652de50ba8efe600ffd3ebd80cc1ca0b2f83a
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-10-07 12:56:16 -0400

    refactor again
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -2,26 +2,26 @@ provider "google" {
   region  = "${var.region}"
 }
 
-# module "orders" {
-#   source  = "app.terraform.io/dotcomrow/orders/google"
-#   version = "> 1.0.0"
-#   project_name = "ordersdomn"
-#   gcp_org_id = "${var.gcp_org_id}"
-#   billing_account = "${var.billing_account}"
-# }
+module "orders" {
+  source  = "app.terraform.io/dotcomrow/orders/google"
+  version = "> 1.0.0"
+  project_name = "order"
+  gcp_org_id = "${var.gcp_org_id}"
+  billing_account = "${var.billing_account}"
+}
 
-# module "cart" {
-#   source  = "app.terraform.io/dotcomrow/cart/google"
-#   version = "> 1.0.0"
-#   project_name = "cartsdomn"
-#   gcp_org_id = "${var.gcp_org_id}"
-#   billing_account ="${var.billing_account}"
-# }
+module "cart" {
+  source  = "app.terraform.io/dotcomrow/cart/google"
+  version = "> 1.0.0"
+  project_name = "cart"
+  gcp_org_id = "${var.gcp_org_id}"
+  billing_account ="${var.billing_account}"
+}
 
-# module "products" {
-#   source  = "app.terraform.io/dotcomrow/products/google"
-#   version = "> 1.0.0"
-#   project_name = "productsdomn"
-#   gcp_org_id = "${var.gcp_org_id}"
-#   billing_account = "${var.billing_account}"
-# }
+module "products" {
+  source  = "app.terraform.io/dotcomrow/products/google"
+  version = "> 1.0.0"
+  project_name = "product"
+  gcp_org_id = "${var.gcp_org_id}"
+  billing_account = "${var.billing_account}"
+}
