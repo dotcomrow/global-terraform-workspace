edc67b88df24cba481065842a91d62fabe2980a3
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-10-17 18:51:27 -0400

    revert
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -7,9 +7,12 @@ terraform {
     cloudflare = {
       source  = "cloudflare/cloudflare"
       version = "~> 4.0"
-      api_token = "${var.cloudflare_token}"
     }
   }
+}
+
+provider "cloudflare" {
+  api_token = "${var.cloudflare_token}"
 }
 
 module "orders" {
