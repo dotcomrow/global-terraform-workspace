b9b7242f69cde4dd0350057b582c77c244b309ac
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-10-19 18:12:32 -0400

    adjusting
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -13,6 +13,23 @@ module "api_gateway" {
   cloudflare_worker_zone_id = "${var.cloudflare_worker_zone_id}"
 }
 
+module "configuration" {
+  source  = "app.terraform.io/dotcomrow/configuration/google"
+  version = "> 1.0.0"
+  project_name = "configuration"
+  project_id = "configuration-${var.suffix}"
+  gcp_org_id = "${var.gcp_org_id}"
+  billing_account = "${var.billing_account}"
+  region  = "${var.region}"
+  bigquery_secret = "${var.bigquery_secret}"
+  python_session_secret = "${var.python_session_secret}"
+  common_project_id = "${var.common_project_id}"
+  audience = "${var.audience}"
+  config_security_group = "${var.config_security_group}"
+  cloudflare_account_id = "${var.cloudflare_account_id}"
+  cloudflare_worker_namespace_id = "${module.api_gateway.api_gateway_namespace_id}"
+}
+
 # module "orders" {
 #   source  = "app.terraform.io/dotcomrow/orders/google"
 #   version = "> 1.0.0"
@@ -54,19 +71,3 @@ module "api_gateway" {
 #   audience = "${var.audience}"
 # }
 
-module "configuration" {
-  source  = "app.terraform.io/dotcomrow/configuration/google"
-  version = "> 1.0.0"
-  project_name = "configuration"
-  project_id = "configuration-${var.suffix}"
-  gcp_org_id = "${var.gcp_org_id}"
-  billing_account = "${var.billing_account}"
-  region  = "${var.region}"
-  bigquery_secret = "${var.bigquery_secret}"
-  python_session_secret = "${var.python_session_secret}"
-  common_project_id = "${var.common_project_id}"
-  audience = "${var.audience}"
-  config_security_group = "${var.config_security_group}"
-  cloudflare_account_id = "${var.cloudflare_account_id}"
-  cloudflare_worker_namespace_id = "${module.api_gateway.api_gateway_namespace_id}"
-}
