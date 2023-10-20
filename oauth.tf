a3866038a8f4cbd549aaaad471d9c394dbdbc6fd
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-10-20 15:32:07 -0400

    uncomment when creating new project
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- oauth.tf
+++ oauth.tf
@@ -1,10 +1,10 @@
-resource "google_iap_brand" "project_brand" {
-  support_email     = "administrator@suncoast.systems"
-  application_title = "Cloud IAP protected Application"
-  project           = var.common_project_id
-}
+# resource "google_iap_brand" "project_brand" {
+#   support_email     = "administrator@suncoast.systems"
+#   application_title = "Cloud IAP protected Application"
+#   project           = var.common_project_id
+# }
 
-resource "google_iap_client" "project_client" {
-  display_name = "UI Client"
-  brand        =  google_iap_brand.project_brand.name
-}
+# resource "google_iap_client" "project_client" {
+#   display_name = "UI Client"
+#   brand        =  google_iap_brand.project_brand.name
+# }
