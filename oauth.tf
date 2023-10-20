be4e5eb62fdbb37c50e01def79713626ae038119
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-10-20 15:13:06 -0400

    oauth test
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

---
+++ oauth.tf
@@ -0,0 +1,8 @@
+resource "google_identity_platform_oauth_idp_config" "oauth_idp_config" {
+  name          = "oidc.oauth-idp-config"
+  display_name  = "domain oauth"
+  client_id     = "client-id"
+  issuer        = "issuer"
+  enabled       = true
+  client_secret = "secret"
+}
