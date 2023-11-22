6e5243d7124b95e32e9551c53b8ce221188b02a0
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-11-22 13:43:01 -0500

    adding delete policy
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- artifact-registry.tf
+++ artifact-registry.tf
@@ -1,10 +1,18 @@
 resource "google_artifact_registry_repository" "registry" {
-  provider      = google-beta
-  repository_id = var.registry_name
-  location      = "us"
-  project       =  var.common_project_id
-  format        = "DOCKER"
+  provider               = google-beta
+  repository_id          = var.registry_name
+  location               = "us"
+  project                = var.common_project_id
+  format                 = "DOCKER"
   cleanup_policy_dry_run = false
+  cleanup_policies {
+    id     = "delete-old-versions"
+    action = "DELETE"
+    condition {
+      package_name_prefixes = ["svc-"]
+      older_than   = "172800s"
+    }
+  }
   cleanup_policies {
     id     = "keep-minimum-versions"
     action = "KEEP"
