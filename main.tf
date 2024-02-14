b19e18be7c377c73c8b9889a586a2dce87a9330a
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-02-14 18:48:50 -0500

    cleanup
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -5,10 +5,3 @@ provider "google" {
 provider "google-beta" {
   region  = "${var.region}"
 }
-
-module "mfe" {
-  source  = "app.terraform.io/dotcomrow/mfe/cloudflare"
-  version = "> 1.0.0"
-  cloudflare_account_id = "${var.cloudflare_account_id}"
-  cloudflare_zone_id = "${var.cloudflare_zone_id}"
-}
